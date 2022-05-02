504c8bbe7e14c5aa69954e43efae6200bb1558b6
carlinhoshk <carlosmdohk@gmail.com>
2022-05-02 17:28:10 -0300

    Arrumando background e subindo fotos e funções de alguns botões

--- app/src/main/java/com/carlos/kalculator/MainActivity.java
+++ app/src/main/java/com/carlos/kalculator/MainActivity.java
@@ -3,14 +3,171 @@ package com.carlos.kalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button numeroZero, numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, numeroSeis, numeroSete, numeroOito,
+    numeroNove, ponto, soma, subtracao, multiplicacao, divisao, igual, botao_limpar, backspace;
+
+    private TextView textExpressao, textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        IniciarComponentes();
         getSupportActionBar().hide();
+
+        numeroZero.setOnClickListener(this);
+        numeroUm.setOnClickListener(this);
+        numeroDois.setOnClickListener(this);
+        numeroTres.setOnClickListener(this);
+        numeroQuatro.setOnClickListener(this);
+        numeroCinco.setOnClickListener(this);
+        numeroSeis.setOnClickListener(this);
+        numeroSete.setOnClickListener(this);
+        numeroOito.setOnClickListener(this);
+        numeroNove.setOnClickListener(this);
+        ponto.setOnClickListener(this);
+        soma.setOnClickListener(this);
+        subtracao.setOnClickListener(this);
+        multiplicacao.setOnClickListener(this);
+        divisao.setOnClickListener(this);
+
+        botao_limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                textExpressao.setText("");
+                textResultado.setText("");
+            }
+        });
+
+        backspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TextView expressao = findViewById(R.id.txt_expressao);
+                String string = expressao.getText().toString();
+
+                if (!string.isEmpty()){
+
+                    byte var0 = 0;
+                    int var1 = string.length()-1;
+                    String txtExpressao = string.substring(var0,var1);
+                    expressao.setText(txtExpressao);
+                }
+                textResultado.setText("");
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        numeroZero = findViewById(R.id.numero_zero);
+        numeroUm = findViewById(R.id.numero_um);
+        numeroDois = findViewById(R.id.numero_dois);
+        numeroTres = findViewById(R.id.numero_tres);
+        numeroQuatro = findViewById(R.id.numero_quatro);
+        numeroCinco = findViewById(R.id.numero_cinco);
+        numeroSeis = findViewById(R.id.numero_seis);
+        numeroSete = findViewById(R.id.numero_sete);
+        numeroOito = findViewById(R.id.numero_oito);
+        numeroNove = findViewById(R.id.numero_nove);
+        ponto = findViewById(R.id.ponto);
+        subtracao = findViewById(R.id.subtracao);
+        multiplicacao = findViewById(R.id.multiplicacao);
+        divisao = findViewById(R.id.divisao);
+        igual = findViewById(R.id.igual);
+        botao_limpar = findViewById(R.id.bt_limpar);
+        textExpressao = findViewById(R.id.txt_expressao);
+        textResultado = findViewById(R.id.txt_resultado);
+        backspace = findViewById(R.id.backspace);
+    }
+
+    public void AcrescentarUmaExpressao(String string,boolean limpar_dados){
+
+        if (textResultado.getText().equals("")){
+            textExpressao.setText("");
+        }
+
+        if (limpar_dados){
+            textResultado.setText("");
+            textExpressao.append(string);
+        }else{
+            textExpressao.append(textResultado.getText());
+            textExpressao.append(string);
+            textResultado.setText("");
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.numero_zero:
+                AcrescentarUmaExpressao("0",true);
+                break;
+
+            case R.id.numero_um:
+                AcrescentarUmaExpressao("1",true);
+                break;
+
+            case R.id.numero_dois:
+                AcrescentarUmaExpressao("2",true);
+                break;
+
+            case R.id.numero_tres:
+                AcrescentarUmaExpressao("3",true);
+                break;
+
+            case R.id.numero_quatro:
+                AcrescentarUmaExpressao("4",true);
+                break;
+
+            case R.id.numero_cinco:
+                AcrescentarUmaExpressao("5",true);
+                break;
+
+            case R.id.numero_seis:
+                AcrescentarUmaExpressao("6",true);
+                break;
+
+            case R.id.numero_sete:
+                AcrescentarUmaExpressao("7",true);
+                break;
+
+            case R.id.numero_oito:
+                AcrescentarUmaExpressao("8",true);
+                break;
+
+            case R.id.numero_nove:
+                AcrescentarUmaExpressao("9",true);
+                break;
+
+            case R.id.ponto:
+                AcrescentarUmaExpressao(".",false);
+                break;
+
+            case R.id.soma:
+                AcrescentarUmaExpressao("+",false);
+                break;
+
+            case R.id.subtracao:
+                AcrescentarUmaExpressao("-",false);
+                break;
+
+            case R.id.multiplicacao:
+                AcrescentarUmaExpressao("*",false);
+                break;
+
+            case R.id.divisao:
+                AcrescentarUmaExpressao("/",false);
+                break;
+        }
+
     }
 }
